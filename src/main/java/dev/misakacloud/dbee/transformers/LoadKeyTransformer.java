1f427ecf5f8f07724e5b7f2493da3142f79cfd12
gzzchh <xjdzch@126.com>
2020-01-30 01:55:43 +0800

    决定直接重定义整个Class

--- src/main/java/dev/misakacloud/dbee/transformers/LoadKeyTransformer.java
+++ src/main/java/dev/misakacloud/dbee/transformers/LoadKeyTransformer.java
@@ -3,11 +3,10 @@ package dev.misakacloud.dbee.transformers;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
-import javassist.bytecode.CodeAttribute;
 
 import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
+import java.util.Arrays;
 
 public class LoadKeyTransformer implements ClassFileTransformer {
     public final String injectedClassName = "com/dbeaver/ee/runtime/lm/DBeaverEnterpriseLM$LicenseKeyProvider";
@@ -17,30 +16,23 @@ public class LoadKeyTransformer implements ClassFileTransformer {
                             ProtectionDomain protectionDomain,
                             byte[] classfileBuffer) {
         className = className.replace("/", ".");
-        if (className.contains("DBeaverEnterpriseLM") && className.contains("LicenseKeyProvider")) {
-            System.out.println("正在加载的类:" + className);
+// && className.contains("LicenseKeyProvider")
+        if (className.contains("DBeaverEnterpriseLM")) {
             CtClass ctclass = null;
-            ClassPool cp = ClassPool.getDefault();
-
-            // 读取自己的包
             try {
-                Class.forName("dev.misakacloud.dbee.utils");
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-            // 写入导入语句
-            cp.importPackage("dev.misakacloud.dbee.utils");
-            try {
-//                ctclass = ClassPool.getDefault().get(className);// 使用全称,用于取得字节码类<使用javassist>
-                CtMethod getKeyMethod = cp.getMethod("DBeaverEnterpriseLM$LicenseKeyProvider", "getDecryptionKey");
-                System.out.println("检测到读取解密Key的行为");
-                getKeyMethod.insertBefore("return new CryptKey().getPublicKey();");
+                ctclass = ClassPool.getDefault().get(className);// 使用全称,用于取得字节码类<使用javassist>
+                System.out.println(ctclass);
+                System.out.println(Arrays.toString(ctclass.getDeclaredMethods()));
+                CtMethod ctmethod = ctclass.getDeclaredMethod("getDecryptionKey");// 得到这方法实例
+                ctmethod.insertBefore("System.out.println(11111111);");
+                // getKeyMethod.insertBefore("return new CryptKey().getPublicKey();");
                 return ctclass.toBytecode();
             } catch (Exception e) {
                 System.out.println(e.getMessage());
                 e.printStackTrace();
             }
         }
+
 //        System.out.println("正在加载的类:" + className);
         return null;
     }
