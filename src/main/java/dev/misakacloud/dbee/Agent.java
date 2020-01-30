bf2db2c2efc81e26d2f25edb56425ed516ad69ef
gzzchh <xjdzch@126.com>
2020-01-30 22:22:46 +0800

    似乎过滤成功

--- src/main/java/dev/misakacloud/dbee/Agent.java
+++ src/main/java/dev/misakacloud/dbee/Agent.java
@@ -1,27 +1,67 @@
 package dev.misakacloud.dbee;
 
-import dev.misakacloud.dbee.transformers.LoadKeyTransformer;
+import dev.misakacloud.dbee.interceptor.LoadKeyInterceptor;
 import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.utility.JavaModule;
 
 import java.lang.instrument.Instrumentation;
-import java.lang.instrument.UnmodifiableClassException;
 
 public class Agent {
 
-    public static void premain(String agentArgs, Instrumentation inst) throws UnmodifiableClassException {
-        System.out.println("Agent已加载");
-        new AgentBuilder.Default()
-                // 类名匹配
-                .type(ElementMatchers.named("org.jkiss.lm.LMEncryption"))
-                .transform((builder, type, classLoader, module) ->
-                        builder.method(ElementMatchers.nameMatches("generatePublicKey"))
-                                .intercept(MethodDelegation.to(LoadKeyTransformer.class)))
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println("this is an perform monitor agent.");
+
+        AgentBuilder.Transformer transformer = new AgentBuilder.Transformer() {
+            @Override
+            public DynamicType.Builder<?> transform(DynamicType.Builder<?> builder,
+                                                    TypeDescription typeDescription,
+                                                    ClassLoader classLoader,
+                                                    JavaModule module) {
+                return builder
+                        .method(ElementMatchers.any()) // 拦截任意方法
+                        .intercept(MethodDelegation.to(LoadKeyInterceptor.class)); // 委托
+            }
+        };
+
+        AgentBuilder.Listener listener = new AgentBuilder.Listener() {
+            @Override
+            public void onDiscovery(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded) {
+
+            }
+
+            @Override
+            public void onTransformation(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module, boolean loaded, DynamicType dynamicType) {
+
+            }
+
+            @Override
+            public void onIgnored(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module, boolean loaded) {
+
+            }
+
+            @Override
+            public void onError(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded, Throwable throwable) {
+
+            }
+
+            @Override
+            public void onComplete(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded) {
+
+            }
+
+        };
+
+        new AgentBuilder
+                .Default()
+                .type(ElementMatchers.nameStartsWith("com.dbeaver.ee.runtime.lm")) // 指定需要拦截的类
+                .transform(transformer)
+                .with(listener)
                 .installOn(inst);
-
-
-
     }
 
 }
--- src/main/java/dev/misakacloud/dbee/interceptor/LoadKeyInterceptor.java
+++ src/main/java/dev/misakacloud/dbee/interceptor/LoadKeyInterceptor.java
@@ -1,24 +1,22 @@
 package dev.misakacloud.dbee.interceptor;
 
-//import com.sun.org.apache.bcel.internal.classfile.Method;
-import javassist.compiler.MemberResolver;
-import net.bytebuddy.asm.Advice;
+
+import net.bytebuddy.implementation.bind.annotation.Origin;
 import net.bytebuddy.implementation.bind.annotation.RuntimeType;
 import net.bytebuddy.implementation.bind.annotation.SuperCall;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.Callable;
 
 public class LoadKeyInterceptor {
     @RuntimeType
-    public static Object intercept(@Advice.Origin MemberResolver.Method method, @SuperCall Callable<?> callable) throws Exception {
-        System.out.println("检测到调用读取解密Key");
+    public static Object intercept(@Origin Method method, @SuperCall Callable<?> callable) throws Exception {
         long start = System.currentTimeMillis();
         try {
-            // 先调用所有
+            // 原有函数执行
             return callable.call();
         } finally {
-            System.out.println("检测到调用读取解密Key");
-            System.out.println(method + " took " + (System.currentTimeMillis() - start) + "ms");
+            System.out.println(method + ": took " + (System.currentTimeMillis() - start) + "ms");
         }
     }
 }
