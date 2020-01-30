c7fa07cb45a853c2c9cb07de5a4a7a04b185ba0e
gzzchh <xjdzch@126.com>
2020-01-30 18:16:32 +0800

    修复命名

--- src/main/java/dev/misakacloud/dbee/Main.java
+++ src/main/java/dev/misakacloud/dbee/Main.java
@@ -10,19 +10,19 @@ import static dev.misakacloud.dbee.utils.OldCryptKey.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String lic = "GcEVPtVH+fzyCX3Jw/b2iDGHIYe20IwwGGzvCaSvgN+SOLyeOfmhTgIXkhhuJsCi7Ov/7Sy2Hpk3\n" +
+        String licenseString = "GcEVPtVH+fzyCX3Jw/b2iDGHIYe20IwwGGzvCaSvgN+SOLyeOfmhTgIXkhhuJsCi7Ov/7Sy2Hpk3\n" +
                 "VdHjehLS727GlKOKKKkZ6s9C8bt+Aw4WEhDsivOJpQt5eLUjvDLhZC0ols4R9kIXHRo1KcS5AaHy\n" +
                 "8EWhdiuxPOJdHTR01waJUvb4RdH8Ldi2m2CNB93sv1OTMvzoDX1oWUnWGN8mL7K0UU+3ksy06a0O\n" +
                 "/AU8wueD1yaXHQp9OML5WmBDZapiuSKoQUH/dPhu6C7XRj1EAiTueNibb9rSfbhlUYKgA/1is4nW\n" +
                 "42xwiN3+jzQrBYO1NQIYAlGHxlsJ0+IxqVLHCw==";
-        lic = lic.replaceAll("\\n", "").trim();
+        licenseString = licenseString.replaceAll("\\n", "").trim();
         Key publicKey = null;
         try {
             publicKey = getKey();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        License licObj = new License(lic, publicKey);
+        License licObj = new License(licenseString, publicKey);
         String licInfo = licObj.toString();
         System.out.println(licInfo);
         licObj.setLicenseType(LMLicenseType.ULTIMATE);
@@ -31,8 +31,8 @@ public class Main {
         licObj.setOwnerName("MisakaCloud");
 //        licObj.setFlags(1);
 //        new MyCryptKey().getPublicKey();
-        String newLic = licObj.getEncryptLicense(new MyCryptKey().getPrivateKey());
-        License licObjNew = new License(newLic, new MyCryptKey().getPublicKey());
-        System.out.println(licObjNew.toString());
+//        String newLic = licObj.getEncryptLicense(new MyCryptKey().getPrivateKey());
+//        License licObjNew = new License(newLic, new MyCryptKey().getPublicKey());
+//        System.out.println(licObjNew.toString());
     }
 }
