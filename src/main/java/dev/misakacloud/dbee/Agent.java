79970bdd12ffba164ef1d42eb6d2b9a74b68f010
gzzchh <xjdzch@126.com>
2020-01-31 22:53:33 +0800

    目前 拦截和自动修改Boot-Class-Path已经完成.Agent中密钥替换已完成

--- src/main/java/dev/misakacloud/dbee/Agent.java
+++ src/main/java/dev/misakacloud/dbee/Agent.java
@@ -12,7 +12,7 @@ import java.lang.instrument.Instrumentation;
 public class Agent {
 
     public static void premain(String agentArgs, Instrumentation inst) {
-        System.out.println("this is an perform monitor agent.");
+        System.out.println("Agent已加载,尝试修改ClassPath");
         AgentBuilder.Listener listener = new AgentBuilder.Listener() {
             @Override
             public void onDiscovery(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded) {
@@ -41,30 +41,12 @@ public class Agent {
         };
         new AgentBuilder
                 .Default()
-                .type(ElementMatchers.nameContains("com.dbeaver.ee.runtime.lm.DBeaverEnterpriseLM$")) // 指定需要拦截的类
+                .type(ElementMatchers.nameContains("com.dbeaver.ee.runtime.lm.DBeaverEnterpriseLM")) // 指定需要拦截的类
                 .transform(Transformers.loadKeyTransformer)
                 .with(listener)
                 .installOn(inst);
     }
 
-    public static void updateClassPath() {
-//        try {
-//            URLClassLoader child = new URLClassLoader (myJar.toURL(), this.getClass().getClassLoader());
-//            Class classToLoad = Class.forName ("com.MyClass", true, child);
-//            Method method = classToLoad.getDeclaredMethod ("myMethod");
-//            Object instance = classToLoad.newInstance ();
-//            Object result = method.invoke (instance);
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (NoSuchMethodException e) {
-//            e.printStackTrace();
-//        } catch (InstantiationException e) {
-//            e.printStackTrace();
-//        } catch (IllegalAccessException e) {
-//            e.printStackTrace();
-//        } catch (InvocationTargetException e) {
-//            e.printStackTrace();
-//        }
-    }
+
 
 }
--- src/main/java/dev/misakacloud/dbee/test.java
+++ src/main/java/dev/misakacloud/dbee/test.java
@@ -1,54 +1,11 @@
 package dev.misakacloud.dbee;
 
 
-import dev.misakacloud.dbee.utils.MyCryptKey;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.util.Base64;
-
 public class test {
-    public static PublicKey getPublicKey(String key) throws Exception {
-        return (PublicKey) new MyCryptKey().getPublicKey();
-//        return new MyCryptKey().getPublicKey();
-    }
 
     public static void main(String[] args) throws Exception {
-//        String lic = "kKGIrBS2fhcXMeMnqblGBdTmnFeMnD9hTIwGAGhJf6myjyNivoKFz1npmv0udI2oIMxdPWdfHMW0P+9+TzYzruK9huqiZmr8WVI91WHt8FnMzV4Sx1BfN1R+DeYdbQEHD+VrGPSvyczCZRtIPLIec3Bk+ELty8qZbjxODNLX3sfGMvUvANjrjydjlOorbmNIAvlw55Iepwuh0EM0RAllkYe6ymE8TjvLQEOzCFhUlSpNkghtgaMGx0hVrYEV0/YVR67ZnCLM1Dzf+CvQTrLgiNqCJ3xyN32DEyLASbVjYdP4YVMp8NVKnsdVorPU5mBuwlwylGwMU/WETka4252bhw==";
-        String lic = "GcEVPtVH+fzyCX3Jw/b2iDGHIYe20IwwGGzvCaSvgN+SOLyeOfmhTgIXkhhuJsCi7Ov/7Sy2Hpk3\n" +
-                "VdHjehLS727GlKOKKKkZ6s9C8bt+Aw4WEhDsivOJpQt5eLUjvDLhZC0ols4R9kIXHRo1KcS5AaHy\n" +
-                "8EWhdiuxPOJdHTR01waJUvb4RdH8Ldi2m2CNB93sv1OTMvzoDX1oWUnWGN8mL7K0UU+3ksy06a0O\n" +
-                "/AU8wueD1yaXHQp9OML5WmBDZapiuSKoQUH/dPhu6C7XRj1EAiTueNibb9rSfbhlUYKgA/1is4nW\n" +
-                "42xwiN3+jzQrBYO1NQIYAlGHxlsJ0+IxqVLHCw==";
-        lic = lic.replaceAll("\\n", "");
-        String publicKeyStr =
-                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk7ciFU/aUCIgH5flBbGD0t7B3KOmfL0l\n" +
-                        "BMf2ENuLA0w/T8A1RvteUYk2EQo3UrZ7kMZ8rK93nmDjituN7jlv/bsxGyAox87BbKYSs9oH5f9P\n" +
-                        "hYHAiTE0PxoMODnl4NgR+Bpc+Ath8wDLHMC+BzYkOy4JQo8EX/ff58TT9UYP8eoDeGdSxQmW3FJC\n" +
-                        "i82UiC5zIk75dx20Al9ql0fdxnzo31q/2MbnNCAfSchsqrKtzBtheex4JvvqZjxn98wk5Te1QgZz\n" +
-                        "Caz4ay9dkLVjSt79QYm5hKb8Jt3O5SxSUsrjmYVeG+k2bQlidw8dENwLZmvJkIJi8kb94yEwY/dq\n" +
-                        "lENDkQIDAQAB";
-        publicKeyStr = publicKeyStr.replaceAll("\\n", "");
-        byte[] decodedBytes = null;
-//        System.out.println(lic.trim());
-        byte[] base64DecodedBytes = Base64.getDecoder().decode(lic.trim());
-        Key publicKey = getPublicKey(publicKeyStr.trim());
-        try {
-            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-            cipher.init(Cipher.DECRYPT_MODE, publicKey);
-            decodedBytes = cipher.doFinal(base64DecodedBytes);
-        } catch (IllegalBlockSizeException | BadPaddingException | NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException e) {
-            e.printStackTrace();
-        }
-        String res = new String(decodedBytes);
-//        System.out.println(res);
-
+        String curPath = System.getProperty("java.class.path");
+        System.out.println(curPath);
 
     }
 }
